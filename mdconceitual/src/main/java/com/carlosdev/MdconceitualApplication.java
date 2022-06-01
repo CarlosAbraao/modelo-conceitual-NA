7cafb3878e79dd861eb2b6dbd6b490d0d189f2f5
Carlos Abraao <carloschavesnd@hotmail.com>
2022-06-01 18:31:52 -0300

    inserindo dados no banco e uso da classe CommandLineRunner

--- mdconceitual/src/main/java/com/carlosdev/MdconceitualApplication.java
+++ mdconceitual/src/main/java/com/carlosdev/MdconceitualApplication.java
@@ -1,13 +1,41 @@
 package com.carlosdev;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.carlosdev.domain.Categoria;
+import com.carlosdev.repository.CategoriaRepository;
+
 @SpringBootApplication
-public class MdconceitualApplication {
+public class MdconceitualApplication implements CommandLineRunner {
+	
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MdconceitualApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria (null, "Eletrodoméstico");
+		
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
+		
+		
+		
+		
 	}
 
 }
